ec1237ddf415eec0b63b7c35f8522ceaeeb2db77
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2025-09-23 08:55:57 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc/Context/SalesWebMvcDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Context
+{
+    public class SalesWebMvcDbContext : DbContext
+    {
+        public SalesWebMvcDbContext(DbContextOptions<SalesWebMvcDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Context;
+using SalesWebMvc.Models;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly SalesWebMvcDbContext _context;
+        public DepartmentsController(SalesWebMvcDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _context.Departments.ToListAsync();
+
+            return View(departments);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var department = await _context.Departments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(department);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete()
+        {
+            return View();
+        }
+    }
+}
